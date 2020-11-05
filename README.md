15818e79cebb0ac162ec809a1a9b7616631d0c9b
Dungngo4520 <dungngo4520@gmail.com>
2020-11-05 23:19:21 +0700

    reconfig firebase

---
+++ README.md
@@ -0,0 +1,5 @@
+# Bài tập lớn nhóm 9: Web chat
+
+## To run this app, go to chatsapp folder and run:
+
+### `npm i`
--- chatsapp/src/firebase.js
+++ chatsapp/src/firebase.js
@@ -1,11 +1,9 @@
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-    apiKey: "AIzaSyCW_uwlo7GlackbhvfxXx3sw8oAGDdw0E4",
-    authDomain: "chatsap-3d28b.firebaseapp.com",
-    databaseURL: "https://chatsap-3d28b.firebaseio.com",
-    projectId: "chatsap-3d28b",
-    storageBucket: "chatsap-3d28b.appspot.com",
-    messagingSenderId: "687831185808",
-    appId: "1:687831185808:web:c5cd2e449bdc246be49b15",
-    measurementId: "G-DE1RRPMS6V"
-  };
+  apiKey: "AIzaSyDib33XpYWpnVBF2aluB3tRqIaHQwTBMzw",
+  authDomain: "chatsapp-5b981.firebaseapp.com",
+  databaseURL: "https://chatsapp-5b981.firebaseio.com",
+  projectId: "chatsapp-5b981",
+  storageBucket: "chatsapp-5b981.appspot.com",
+  messagingSenderId: "899432261906",
+  appId: "1:899432261906:web:9c0b0d3392776a9c176bcb"
+};
